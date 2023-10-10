c8596cfcdce9b702dd7588ff371fa39dc7a1ca7e
DanielSiilva <daniellsousa0614@gmail.com>
2023-10-10 09:12:05 -0300

    Feat: adding safeareacontext hook and base page

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {theme} from './src/theme/theme';
 import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <LoginScreen />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 export default App;
