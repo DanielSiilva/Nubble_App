4ae527725f0bef4c44c596cd0f97748612447bd8
DanielSiilva <daniellsousa0614@gmail.com>
2023-10-29 22:35:04 -0300

    PosItem 1 - header and image

---
+++ src/components/PostItem/components/PostImage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Dimensions, Image} from 'react-native';
+
+import {Post} from '@domain';
+
+type Props = Pick<Post, 'imageURL'>;
+export function PostImage({imageURL}: Props) {
+  return (
+    <Image
+      source={{uri: imageURL}}
+      resizeMode="cover"
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{width: Dimensions.get('screen').width, height: 300}}
+    />
+  );
+}
