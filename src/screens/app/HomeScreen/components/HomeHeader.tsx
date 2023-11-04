c053068114c4dc8ce2e9be32aea4330a201e732f
DanielSiilva <daniellsousa0614@gmail.com>
2023-11-04 09:00:47 -0300

    logo app

--- src/screens/app/HomeScreen/components/HomeHeader.tsx
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {SimpleLogo} from '@brand';
+
 import {Box, BoxProps, Icon} from '@components';
 import {useAppSafeArea} from '@hooks';
 
@@ -8,7 +10,7 @@ export function HomeHeader() {
 
   return (
     <Box {...$wrapper} style={{paddingTop: top}}>
-      <Box backgroundColor="carrotSecondary" height={16} width={70} />
+      <SimpleLogo width={70} />
       <Box flexDirection="row">
         <Box mr="s24">
           <Icon name="search" />
@@ -21,10 +23,8 @@ export function HomeHeader() {
     </Box>
   );
 }
-
 const $wrapper: BoxProps = {
   flexDirection: 'row',
-
   justifyContent: 'space-between',
   paddingBottom: 's24',
   paddingHorizontal: 's24',
