6925ce4d050a2ada6a7be4a1974dc80a68196c06
DanielSiilva <daniellsousa0614@gmail.com>
2023-11-15 11:07:03 -0300

    integração fetch API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,7 +2,18 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Nw.ijprJVvOtayMRA1lcNNUFouZhP11cyLSc0H4L2gvy8HtrPsGqxWIhwX65Bsx',
+    },
+  });
+
+  let data = await response.json();
+  console.log('FETCH DATA:', data);
+
+  // await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
 
