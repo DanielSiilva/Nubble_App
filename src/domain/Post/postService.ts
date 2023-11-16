b5befb10a1a4197172ae8b67c68970fe32146188
DanielSiilva <daniellsousa0614@gmail.com>
2023-11-15 22:29:59 -0300

    Estados de Loading, Error e Lista vazia

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,10 +1,11 @@
 import {postAdapter} from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
-
 async function getList(): Promise<Post[]> {
   const postPageAPI = await postApi.getList();
 
+  // throw new Error('Erro de teste');
+  // return [];
   return postPageAPI.data.map(postAdapter.toPost);
 }
 
