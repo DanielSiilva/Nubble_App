a447e0e69b5f38af09654ef3eb2a1f5c474f29a3
DanielSiilva <daniellsousa0614@gmail.com>
2023-12-02 21:54:51 -0300

    Estado Global (Zustand)

--- App.tsx
+++ App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
-import {ToastProvider} from '@services';
+// import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
-import {Router} from '@routes';
 
+import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <ToastProvider>
-          <Router />
-          <Toast />
-        </ToastProvider>
+        {/* Only use ToastProvider if it is using Context implementation.
+          Zustand implementation doesn't need a provider */}
+        {/* <ToastProvider> */}
+        <Router />
+        <Toast />
+        {/* </ToastProvider> */}
       </ThemeProvider>
     </SafeAreaProvider>
   );
 }
-
 export default App;
