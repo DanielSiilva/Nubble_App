05b16de49e43da6469e6511b14869495a4399ff5
DanielSiilva <daniellsousa0614@gmail.com>
2023-12-03 22:20:21 -0300

    ProfileScreen

--- src/screens/app/ProfileScreen/ProfileScreen.tsx
+++ src/screens/app/ProfileScreen/ProfileScreen.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 
-import {Box, ProfileAvatar, Screen, Text} from '@components';
+import {useUserGetById} from '@domain';
+
+import {ActivityIndicator, Box, ProfileAvatar, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function ProfileScreen({route}: AppScreenProps<'ProfileScreen'>) {
   const userId = route.params.userId;
 
+  const {error, loading, user} = useUserGetById(userId);
+
   return (
     <Screen canGoBack>
-      <Box alignContent="center">
-        <ProfileAvatar imageUrl="exemplo" size={64} borderRadius={24} />
-        <Text preset="headingMedium" bold />
-        <Text>@</Text>
-      </Box>
+      {loading && <ActivityIndicator />}
+      {error && <Text> error ao carregar perfil do usuário</Text>}
+
+      {user && (
+        <Box alignContent="center">
+          <ProfileAvatar
+            imageUrl={user.profileUrl}
+            size={64}
+            borderRadius={24}
+          />
+          <Text preset="headingMedium" bold>
+            {user.fullName}
+          </Text>
+          <Text>@{user.username}</Text>
+        </Box>
+      )}
     </Screen>
   );
 }
