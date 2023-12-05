fb4a02905b5f05fc34203c817b7c0cdab715a1bc
DanielSiilva <daniellsousa0614@gmail.com>
2023-12-04 21:51:40 -0300

    Cache e Stale time

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -4,29 +4,10 @@ import {useQuery} from '@tanstack/react-query';
 import {userService} from '../userService';
 
 export function useUserGetById(id: number) {
-  /*const [user, setUser] = useState<User>();
-  const [error, setError] = useState<boolean | null>(null);
-  const [loading, setLoading] = useState(false);
-
-  const getUserById = useCallback(async () => {
-    try {
-      setLoading(true);
-      const _user = await userService.getById(id);
-      setUser(_user);
-    } catch (er) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  }, [id]);
-
-  useEffect(() => {
-    getUserById();
-  }, [getUserById]); */
-
   const {data, isLoading, isError} = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
+    staleTime: 1000 * 30, // 10 seconds
   });
 
   return {
