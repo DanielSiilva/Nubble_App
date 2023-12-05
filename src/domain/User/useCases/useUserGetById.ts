5e878649c5d12c357eb9b8c29d12ec9077a383e0
DanielSiilva <daniellsousa0614@gmail.com>
2023-12-04 21:30:16 -0300

    setup and useQuery

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -1,10 +1,10 @@
-import {useCallback, useEffect, useState} from 'react';
+import {QueryKeys} from '@infra';
+import {useQuery} from '@tanstack/react-query';
 
 import {userService} from '../userService';
-import {User} from '../userTypes';
 
 export function useUserGetById(id: number) {
-  const [user, setUser] = useState<User>();
+  /*const [user, setUser] = useState<User>();
   const [error, setError] = useState<boolean | null>(null);
   const [loading, setLoading] = useState(false);
 
@@ -22,11 +22,16 @@ export function useUserGetById(id: number) {
 
   useEffect(() => {
     getUserById();
-  }, [getUserById]);
+  }, [getUserById]); */
+
+  const {data, isLoading, isError} = useQuery({
+    queryKey: [QueryKeys.UserGetById, id],
+    queryFn: () => userService.getById(id),
+  });
 
   return {
-    user,
-    error,
-    loading,
+    user: data,
+    isError,
+    isLoading,
   };
 }
